15e784ac9f6f58d3ed77cc6b649328fd9239402a
Simonluca Pisano <masterlemon98@outlook.com>
2026-04-19 18:05:50 +0200

    Implement Dynamic Pages for Courses Gallery

---
+++ src/data/courses.js
@@ -0,0 +1,23 @@
+export const courses = [
+    {
+        slug: "corso-barman",
+        period: "Marzo 2016",
+        title: "Barman / Bartender / Flair",
+        desc: "Corso di formazione sulla preparazione e il servizio dei cocktail e delle bevande alcoliche e non",
+        image: "/assets/Drinxology.jpg"
+    },
+    {
+        slug: "corso-alimentarista",
+        period: "Maggio 2016",
+        title: "HACCP",
+        desc: "Corso di formazione per l’applicazione delle norme UNI ENI ISO e nozioni di base dell'HACCP",
+        image: "/assets/HACCP.jpg"
+    },
+    {
+        slug: "corso-orogel",
+        period: "Maggio 2017",
+        title: "Orogel",
+        desc: "Legumi surgelati; Tecniche di coltura, stagionalità della produzione, processi di trattamento industriali",
+        image: "/assets/Orogel.jpg"
+    }
+];
